b75fb86d41ecae6c4322ec5db139ffc8f6bab98b
Pedro-Costa123 <pedrocostaalves@live.com.pt>
2022-12-30 23:12:33 +0000

    Section 12 : 152

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import './App.css';
+import "./App.css";
+import Button from "./components/UI/Button/Button";
+import DemoOutput from "./components/UI/Button/Demo/DemoOutput";
 
 function App() {
+  const [showPar, setShowPar] = useState(false);
+
+  console.log("App Runnig");
+
+  const toggleParHandler = () => {
+    setShowPar((prevShowPar) => !prevShowPar);
+  };
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      <DemoOutput show={showPar}/>
+      <Button onClick={toggleParHandler}>Toggle Paragraph</Button>
     </div>
   );
 }

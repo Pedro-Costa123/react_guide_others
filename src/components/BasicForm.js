56c15888122a2821bbf91b5612c0a083c0392336
Pedro-Costa123 <pedrocostaalves@live.com.pt>
2023-01-30 16:29:15 +0000

    Section 16 : 210

--- src/components/BasicForm.js
+++ src/components/BasicForm.js
@@ -1,22 +1,133 @@
+import React from "react";
+
+import useInput from "../hooks/use-input";
+
 const BasicForm = (props) => {
+  //First Name
+  const {
+    value: enteredFirstName,
+    isValid: enteredFirstNameIsValid,
+    hasError: firstNameInputHasError,
+    valueHandler: firstNameInputHandler,
+    inputBlurHandler: firstNameInputBlurHandler,
+    reset: firstNameReset,
+  } = useInput((value) => value.trim() !== "");
+
+  const firstNameInputClasses = firstNameInputHasError
+    ? "form-control invalid"
+    : "form-control";
+
+  //Last Name
+  const {
+    value: enteredLastName,
+    isValid: enteredLastNameIsValid,
+    hasError: lastNameInputHasError,
+    valueHandler: lastNameInputHandler,
+    inputBlurHandler: lastNameInputBlurHandler,
+    reset: lastNameReset,
+  } = useInput((value) => value.trim() !== "");
+
+  const lastNameInputClasses = lastNameInputHasError
+    ? "form-control invalid"
+    : "form-control";
+
+  //E-mail
+  const {
+    value: enteredEmail,
+    isValid: enteredEmailIsValid,
+    hasError: emailInputHasError,
+    valueHandler: emailInputHandler,
+    inputBlurHandler: emailInputBlurHandler,
+    reset: emailReset,
+  } = useInput((value) =>
+    value
+      .trim()
+      .match(
+        /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+      )
+  );
+
+  const emailInputClasses = emailInputHasError
+    ? "form-control invalid"
+    : "form-control";
+
+  //Form
+  let formVal = false;
+
+  if (
+    enteredFirstNameIsValid &&
+    enteredLastNameIsValid &&
+    enteredEmailIsValid
+  ) {
+    formVal = true;
+  }
+
+  const formSubmissionHandler = (event) => {
+    event.preventDefault();
+
+    if (
+      !enteredFirstNameIsValid ||
+      !enteredLastNameIsValid ||
+      !enteredEmailIsValid
+    ) {
+      return;
+    }
+
+    console.log(enteredFirstName);
+    console.log(enteredLastName);
+    console.log(enteredEmail);
+
+    firstNameReset();
+    lastNameReset();
+    emailReset();
+  };
+
+  //
   return (
-    <form>
+    <form onSubmit={formSubmissionHandler}>
       <div className="control-group">
-        <div className="form-control">
-          <label htmlFor="name">First Name</label>
-          <input type="text" id="name" />
+        <div className={firstNameInputClasses}>
+          <label htmlFor="fname">First Name</label>
+          <input
+            type="text"
+            id="fname"
+            onChange={firstNameInputHandler}
+            onBlur={firstNameInputBlurHandler}
+            value={enteredFirstName}
+          />
+          {firstNameInputHasError && (
+            <p className="error-text">First name must not be empty</p>
+          )}
         </div>
-        <div className="form-control">
-          <label htmlFor="name">Last Name</label>
-          <input type="text" id="name" />
+        <div className={lastNameInputClasses}>
+          <label htmlFor="lname">Last Name</label>
+          <input
+            type="text"
+            id="lname"
+            onChange={lastNameInputHandler}
+            onBlur={lastNameInputBlurHandler}
+            value={enteredLastName}
+          />
+          {lastNameInputHasError && (
+            <p className="error-text">Last name must not be empty</p>
+          )}
         </div>
       </div>
-      <div className="form-control">
-        <label htmlFor="name">E-Mail Address</label>
-        <input type="text" id="name" />
+      <div className={emailInputClasses}>
+        <label htmlFor="email">E-Mail Address</label>
+        <input
+          type="email"
+          id="email"
+          onChange={emailInputHandler}
+          onBlur={emailInputBlurHandler}
+          value={enteredEmail}
+        />
+        {emailInputHasError && (
+          <p className="error-text">Enter a valid email address</p>
+        )}
       </div>
       <div className="form-actions">
-        <button>Submit</button>
+        <button disabled={!formVal}>Submit</button>
       </div>
     </form>
   );
